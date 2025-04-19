283d560f771d6647f99b8cb638765e7eda3b6416
tharindu <tharindu@spil.com.au>
2025-04-19 09:39:08 +0530

    Add Theme And Column-Row Numbers

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-use iced::widget::{container, text_editor};
-use iced::widget::Text;
-use iced::{Element, Sandbox, Settings};
+use iced::widget::horizontal_space;
+use iced::widget::keyed::column;
+use iced::widget::row;
+use iced::widget::{column, container, text, text_editor};
+use iced::Length;
+use iced::{Element, Sandbox, Settings, Theme};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
 struct Editor {
-    content: text_editor::Content
-} 
+    content: text_editor::Content,
+}
 
 #[derive(Debug, Clone)]
 enum Message {
-    Edit(text_editor::Action)
+    Edit(text_editor::Action),
 }
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self{
-            content: text_editor::Content::default()
+        Self {
+            content: text_editor::Content::with(include_str!("main.rs")),
         }
     }
 
     fn title(&self) -> String {
         String::from("Editor")
-    }    
+    }
 
     fn update(&mut self, message: Message) {
         match message {
@@ -36,8 +39,20 @@ impl Sandbox for Editor {
         }
     }
 
-    fn view(&self) -> Element<'_,Message> {
-       let input = text_editor(&self.content).on_edit(Message::Edit);
-       container(input).padding(10) .into()         
+    fn view(&self) -> Element<'_, Message> {
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+
+        let position = {
+            let (line, column) = self.content.cursor_position();
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+        let status_bar = row![horizontal_space(Length::Fill), position];
+        container(column![input, status_bar].spacing(10))
+            .padding(10)
+            .into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
